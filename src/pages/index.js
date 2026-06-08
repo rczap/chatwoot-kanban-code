59055b2aa4f0c4d7bceda089f248ddd1240e9808
rczap <137716964+rczap@users.noreply.github.com>
2026-06-08 17:00:31 -0300

    Create index.js

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ padding: '40px', fontFamily: 'sans-serif', textAlign: 'center' }}>
+      <h1>Chatwoot Kanban</h1>
+      <p>O sistema está conectando ao banco de dados...</p>
+    </div>
+  )
+}
